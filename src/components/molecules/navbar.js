f3dac80cce790ee22d3d5eb026be0dbb1b38c266
Daniel <daniel.laskowski94@gmail.com>
2021-04-10 22:30:22 +0200

    add new items

--- src/components/molecules/navbar.js
+++ src/components/molecules/navbar.js
@@ -16,7 +16,12 @@ const StyledNav = styled.nav`
     left: 0;
     top: 100%;
     margin: 0;
-    padding: 0;
+    padding-bottom: 3px;
+  }
+  li ul li {
+    padding: 7px 21px;
+    min-width: 226px;
+    color: ${({ theme }) => theme.color.black};
   }
   li:hover > ul {
     display: block;
@@ -27,16 +32,33 @@ const Navbar = () => {
   return (
     <StyledNav>
       <ul>
-        <MenuItem text="Artykuły" slug="/artykuly" />
-        <MenuItem text="Dobre praktyki" slug="/dobre-praktyki" />
-        <MenuItem text="Baza firm" slug="/baza-firm" />
-        <MenuItem text="B CORP" slug="/b-corp" />
-        <MenuItem text="Wydarzenia" slug="/wydarzenia" />
-        <SubmenuItem text="Biblioteka" slug="/biblioteka">
-          <MenuItem text="Księgarnia" slug="/ksiegarnia" />
-          <MenuItem text="Publikacje/Raporty" slug="/publikacje-i-raporty" />
+        <SubmenuItem text="Teksty" slug="/teksty">
+          <MenuItem text="Wywiady" slug="/wywiady" />
+          <MenuItem text="Artykuły" slug="/artykuly" />
         </SubmenuItem>
-        <MenuItem text="Navigate in english" slug="/english" />
+        <SubmenuItem text="Aktualności" slug="/aktualnosci">
+          <MenuItem text="Dobre praktyki" slug="/dobre-praktyki" />
+          <MenuItem text="Wydarzenia" slug="/wydarzenia" />
+        </SubmenuItem>
+        <MenuItem text="Baza firm" slug="#" />
+        <SubmenuItem text="B Corp" slug="/b-corp">
+          <MenuItem text="Czym są B Corpy?" slug="/czym-sa-b-corpy" />
+          <MenuItem text="Jak zostać B Corpem" slug="/jak-zostac-b-corpem" />
+          <MenuItem text="Baza polskich B Corpów" slug="/bcorp" />
+          <MenuItem text="Teksty o B Corpach" slug="/teksty-o-b-corpach" />
+        </SubmenuItem>
+        <SubmenuItem text="Polecamy" slug="/polecamy">
+          <MenuItem text="Publikacje/raporty" slug="/publikacje-i-raporty" />
+          <MenuItem text="Księgarnia" slug="/ksiegarnia" />
+        </SubmenuItem>
+        <SubmenuItem text="Navigator in English" slug="/navigator-in-english">
+          <MenuItem text="B Corps in Poland" slug="/b-corps-in-poland" />
+          <MenuItem text="Sustainbility on CEE" slug="/sustainbility-on-cee" />
+          <MenuItem
+            text="Articles and Interviews"
+            slug="/articles-and-interviews"
+          />
+        </SubmenuItem>
       </ul>
     </StyledNav>
   );
