22020b4d87a8fb5439d2df26d8d43b451a17865d
Daniel <daniel.laskowski94@gmail.com>
2021-04-18 19:25:41 +0200

    add search config

---
+++ src/utils/algolia-queries.js
@@ -0,0 +1,45 @@
+const postQuery = `
+{
+    posts: allWpPost {
+      nodes {
+        objectID:id
+        categories {
+          nodes {
+            name
+            slug
+            wpChildren {
+              nodes {
+                name
+                slug
+              }
+            }
+          }
+        }
+        title
+        excerpt
+        slug
+        dateGmt
+        tags {
+          nodes {
+            name
+            slug
+          }
+        }
+      }
+    }
+  }
+`;
+
+const queries = [
+  {
+    query: postQuery,
+    transformer: ({ data }) => data.posts.nodes,
+
+    settings: {
+      attributesToSnippet: [`excerpt:20`],
+    },
+    matchFields: ["dateGmt"],
+  },
+];
+
+module.exports = queries;
