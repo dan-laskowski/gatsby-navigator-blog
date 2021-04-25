850a8b073769a2293cc73634bb68c328130dafd6
Daniel <daniel.laskowski94@gmail.com>
2021-04-25 19:32:47 +0200

    change default meta tag image

--- src/components/molecules/seo.js
+++ src/components/molecules/seo.js
@@ -4,7 +4,12 @@ import truncate from "utils/truncate";
 import { window } from "browser-monads";
 
 const Seo = ({ title, description, image }) => {
-  const tagImage = image ? image : `./assets/images/OGLogo.png`;
+  if (typeof image === "undefined") {
+    image = `https://i.imgur.com/ElSdyuJ.png`;
+  }
+  console.log(title);
+  console.log(description);
+  console.log(image);
   return (
     <Helmet>
       <title>{title}</title>
@@ -13,12 +18,12 @@ const Seo = ({ title, description, image }) => {
       <meta property="og:url" content={window.location.href} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={tagImage} />
+      <meta property="og:image" content={image} />
       <meta property="twitter:card" content="summary_large_image" />
       <meta property="twitter:url" content={window.location.href} />
       <meta property="twitter:title" content={title} />
       <meta property="twitter:description" content={description} />
-      <meta property="twitter:image" content={tagImage} />
+      <meta property="twitter:image" content={image} />
     </Helmet>
   );
 };
