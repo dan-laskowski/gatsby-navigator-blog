c0f9df1f62f897e0a0b083ca394fbb688961e37a
Daniel <daniel.laskowski94@gmail.com>
2021-05-04 15:58:13 +0200

    add tag tablet mediaquery

--- src/templates/tag.js
+++ src/templates/tag.js
@@ -11,15 +11,23 @@ import PostSmall from "molecules/postSmall";
 import Layout from "organisms/layout";
 import truncate from "utils/truncate";
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  min-height: 100vh;
+`;
 
 const ContentWrapper = styled.div`
   display: grid;
   justify-content: center;
-  grid-template-columns: repeat(12, 122px);
-  grid-gap: 16px;
+  grid-template-columns: repeat(12, 1fr);
+  column-gap: 40px;
+  max-width: 1645px;
   grid-template-rows: auto;
-  grid-template-areas: "text text text text space img img img img aside aside aside";
+  margin: 0 auto;
+  @media only screen and (max-width: 1745px) {
+    margin-left: 30px;
+    margin-right: 30px;
+    column-gap: 30px;
+  }
 `;
 const CategoryName = styled.div`
   background: ${({ theme }) => theme.color.lightGray};
@@ -32,15 +40,29 @@ const CategoryName = styled.div`
   *:visited {
     color: ${({ theme }) => theme.color.black};
   }
+  @media only screen and (max-width: 1370px) {
+    height: 320px;
+  }
+  @media only screen and (max-width: 880px) {
+    height: 160px;
+    h1 {
+      font-size: 24px;
+      line-height: 29px;
+    }
+  }
 `;
 const StyledHeading = styled(Heading)`
   font-size: ${({ theme }) => theme.font.heading.size};
   text-transform: uppercase;
   margin-bottom: 26px;
+  @media only screen and (max-width: 880px) {
+    margin-bottom: 12px;
+    margin-top: 0px;
+  }
 `;
-const Posts = styled.div`
-  grid-column-start: text;
-  grid-column-end: img;
+const PostsWrapper = styled.div`
+  grid-column-start: 1;
+  grid-column-end: 10;
   a:first-child {
     margin-top: 0;
   }
@@ -50,22 +72,99 @@ const Posts = styled.div`
   }
 `;
 const StyledPostLarge = styled(PostLarge)`
+  display: grid;
+  justify-content: center;
+  grid-template-columns: repeat(9, 1fr);
+  column-gap: 16px;
+  margin-top: 28px;
+  .text {
+    grid-column-start: 1;
+    grid-column-end: 5;
+  }
+  .image {
+    grid-column-start: 6;
+    grid-column-end: 10;
+  }
   h1 {
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
     font-size: 28px;
     line-height: 33px;
   }
   h2 {
+    display: -webkit-box;
+    -webkit-box-orient: vertical;
+    overflow: hidden;
     font-size: 20px;
     line-height: 34px;
   }
-  .gatsby-image-wrapper {
-    max-width: 572px;
-    height: 320px;
-  }
   padding-bottom: 28px;
+  @media only screen and (max-width: 1600px) {
+    h1 {
+      font-size: 24px;
+      line-height: 28px;
+    }
+    h2 {
+      display: -webkit-box;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 3;
+      overflow: hidden;
+      font-size: 18px;
+      line-height: 28px;
+    }
+  }
+  @media only screen and (max-width: 1370px) {
+    h1 {
+      font-size: 18px;
+      line-height: 20px;
+      margin-bottom: 10px;
+    }
+    h2 {
+      font-size: 12px;
+      line-height: 15px;
+      -webkit-line-clamp: 4;
+      margin-bottom: 14px;
+    }
+  }
+  @media only screen and (max-width: 1040px) {
+    h1 {
+      -webkit-line-clamp: 2;
+    }
+    h2 {
+      -webkit-line-clamp: 3;
+    }
+  }
+  @media only screen and (max-width: 946px) {
+    grid-template-columns: repeat(4, 1fr);
+    column-gap: 30px;
+    .text {
+      grid-column-start: 1;
+      grid-column-end: 3;
+    }
+    .image {
+      grid-column-start: 3;
+      grid-column-end: 5;
+    }
+    padding-bottom: 10px;
+  }
+  @media only screen and (max-width: 720px) {
+    .text {
+      grid-column-start: 1;
+      grid-column-end: 3;
+    }
+    .image {
+      grid-column-start: 3;
+      grid-column-end: 6;
+    }
+  }
 `;
 const StyledAside = styled(Aside)`
-  grid-area: aside;
+  grid-column-start: 10;
+  grid-column-end: 13;
+  @media only screen and (max-width: 720px) {
+    display: none;
+  }
 `;
 
 const Tag = ({ data: { wpTag, allWpPost, asideQuery }, pageContext }) => {
@@ -84,7 +183,7 @@ const Tag = ({ data: { wpTag, allWpPost, asideQuery }, pageContext }) => {
           </Link>
         </CategoryName>
         <ContentWrapper>
-          <Posts>
+          <PostsWrapper>
             {allWpPost.edges.map(({ node }) => (
               <StyledPostLarge
                 key={node.title}
@@ -96,7 +195,7 @@ const Tag = ({ data: { wpTag, allWpPost, asideQuery }, pageContext }) => {
                 slug={node.slug}
               />
             ))}
-          </Posts>
+          </PostsWrapper>
           <StyledAside>
             <AsideSection title="ostatnie" to={`/`}>
               {asideQuery.nodes.map(node => (
