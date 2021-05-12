ac0c5c3e5642bd18a0f97d5529d8c2bb79aa8ffc
Daniel <daniel.laskowski94@gmail.com>
2021-05-12 08:57:09 +0200

    replace react with preact

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,6 +76,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-preact`,
     `gatsby-plugin-gatsby-cloud`,
   ],
 };
