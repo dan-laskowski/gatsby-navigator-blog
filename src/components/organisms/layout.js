f72e2f30eb1e550a816e0e8256cdc2682b63d18f
Daniel <daniel.laskowski94@gmail.com>
2021-05-12 19:04:43 +0200

    add webfonts

--- src/components/organisms/layout.js
+++ src/components/organisms/layout.js
@@ -1,4 +1,8 @@
 import React from "react";
+import "@fontsource/chivo/700.css";
+import "@fontsource/chivo/400.css";
+import "@fontsource/inria-serif/700.css";
+import "@fontsource/inria-serif/400.css";
 import { ThemeProvider } from "styled-components";
 import { Reset } from "styled-reset";
 import GlobalStyle from "assets/styles/globalStyle";
