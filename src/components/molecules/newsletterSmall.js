351eac6ae40b44ca8d4831c5d672abcd179ecab5
Daniel <daniel.laskowski94@gmail.com>
2021-07-15 15:03:51 +0200

    update media queries

---
+++ src/components/molecules/newsletterSmall.js
@@ -0,0 +1,58 @@
+import React from "react";
+import styled from "styled-components";
+import { Heading, Subheading } from "atoms/heading";
+import { Link } from "gatsby";
+
+const Wrapper = styled.section`
+  display: none;
+  content-visibility: hidden;
+  @media only screen and (max-width: 600px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    content-visibility: auto;
+  }
+`;
+
+const Title = styled(Heading)`
+  color: ${({ theme }) => theme.color.orange};
+  margin-bottom: 6px;
+`;
+
+const StyledSubheading = styled(Subheading)`
+  color: ${({ theme }) => theme.color.black};
+  font-size: 12px;
+  line-height: 14px;
+  margin-bottom: 21px;
+  text-transform: uppercase;
+  font-family: ${({ theme }) => theme.font.heading.family};
+`;
+
+const StyledLink = styled(Link)`
+  display: block;
+  font-size: 20px;
+  line-height: 25px;
+  font-weight: bold;
+  max-width: 380px;
+  width: calc(100% - 48px);
+  padding: 6px 0;
+  margin: 0 24px;
+  margin-bottom: 40px;
+  text-align: center;
+  color: ${({ theme }) => theme.color.offWhite};
+  background: ${({ theme }) => theme.color.orange};
+`;
+
+const NewsletterSmall = () => {
+  return (
+    <Wrapper>
+      <Title text="Newsletter" />
+      <StyledSubheading aria-label="Bądź na bieżąco" text="Bądź na bieżąco" />
+      <StyledLink to="/newsletter" aria-label="Zapisz się">
+        Zapisz się
+      </StyledLink>
+    </Wrapper>
+  );
+};
+
+export default NewsletterSmall;
--- src/templates/category.js
+++ src/templates/category.js
@@ -8,6 +8,7 @@ import Aside from "organisms/aside";
 import AsideSection from "molecules/asideSection";
 import Layout from "organisms/layout";
 import Post from "molecules/post";
+import NewsletterSmall from "molecules/newsletterSmall";
 
 const Wrapper = styled.div`
   min-height: 100vh;
@@ -50,15 +51,13 @@ const CategoryName = styled.div`
   *:link {
     color: ${({ theme }) => theme.color.black};
   }
-  @media only screen and (max-width: 1370px) {
-    height: 320px;
+  @media only screen and (max-width: 1180px) {
+    height: 100px;
   }
-  @media only screen and (max-width: 880px) {
-    height: 160px;
-    h1 {
-      font-size: 24px;
-      line-height: 29px;
-    }
+  @media only screen and (max-width: 600px) {
+    min-height: 180px;
+    height: auto;
+    margin-bottom: 20px;
   }
 `;
 const SubcategoryName = styled.div`
@@ -70,37 +69,144 @@ const SubcategoryName = styled.div`
   *:not(:last-child) {
     margin-right: 40px;
   }
-  @media only screen and (max-width: 880px) {
-    font-size: 10px;
-    line-height: 12px;
+  @media only screen and (max-width: 1180px) {
+    font-size: 12px;
+    line-height: 16px;
+  }
+  @media only screen and (max-width: 600px) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    * {
+      margin-bottom: 20px;
+    }
+    *:not(:last-child) {
+      margin-right: 0px;
+    }
   }
 `;
 const StyledHeading = styled(Heading)`
   font-size: 60px;
-  margin-bottom: 22px;
+  line-height: 71px;
+  margin-top: 0;
+  margin-bottom: 24px;
   text-transform: uppercase;
-  @media only screen and (max-width: 880px) {
-    margin-bottom: 12px;
-    margin-top: 0px;
+  @media only screen and (max-width: 1180px) {
+    font-size: 25px;
+    line-height: 30px;
+    margin-bottom: 8px;
+  }
+  @media only screen and (max-width: 600px) {
+    font-size: 26px;
+    line-height: 31px;
+    margin-top: 20px;
+    margin-bottom: 20px;
   }
 `;
 const PostsWrapper = styled.div`
   grid-column-start: 1;
   grid-column-end: 10;
-  a:first-child {
+  padding-bottom: 52px;
+  border-bottom: 1px solid ${({ theme }) => theme.color.lightGray};
+  a:first-child article {
     margin-top: 0;
+    padding-top: 0;
   }
-  a:last-child {
+  a:last-child article {
     border-bottom: none;
   }
   @media only screen and (max-width: 720px) {
     grid-column-start: 1;
     grid-column-end: 13;
+    border-bottom: none;
+    padding-bottom: 40px;
+  }
+`;
+const PostCategory = styled(Post)`
+  .article {
+    padding-top: 20px;
+    padding-bottom: 20px;
+  }
+  .category {
+    margin-top: 6px;
+    margin-bottom: 6px;
+  }
+  .title {
+    margin-bottom: 32px;
+  }
+  .subtitle {
+    margin-bottom: 12px;
+  }
+
+  @media only screen and (max-width: 1100px) {
+    .title {
+      margin-bottom: 22px;
+      -webkit-line-clamp: 2;
+    }
+    .subtitle {
+      font-size: 16px;
+      line-height: 18px;
+      -webkit-line-clamp: 4;
+    }
+  }
+  @media only screen and (max-width: 900px) {
+    .article {
+      padding-top: 14px;
+      padding-bottom: 14px;
+      border-bottom: none;
+    }
+    .category {
+      font-size: 12px;
+      line-height: 14px;
+      margin-bottom: 0;
+    }
+    .title {
+      font-size: 18px;
+      line-height: 20px;
+      margin-bottom: 6px;
+    }
+    .subtitle {
+      font-size: 12px;
+      line-height: 15px;
+      -webkit-line-clamp: 5;
+    }
+  }
+  @media only screen and (max-width: 600px) {
+    .article {
+      display: flex;
+      flex-direction: column;
+      padding-top: 20px;
+      padding-bottom: 20px;
+    }
+    .category {
+      margin-top: 10px;
+      margin-bottom: 6px;
+    }
+    .title {
+      font-size: 20px;
+      line-height: 24px;
+      -webkit-line-clamp: 3;
+      margin-bottom: 10px;
+    }
+    .subtitle {
+      display: -webkit-box;
+      font-size: 14px;
+      line-height: 18px;
+      -webkit-line-clamp: 3;
+      margin-bottom: 10px;
+    }
   }
 `;
 const StyledAside = styled(Aside)`
   grid-column-start: 10;
   grid-column-end: 13;
+  margin-top: -19px;
+  section {
+    margin-bottom: 20px;
+  }
+  .tags {
+    margin-bottom: 14px;
+  }
   @media only screen and (max-width: 720px) {
     display: none;
   }
@@ -148,10 +254,23 @@ const AsidePost = styled(Post)`
     }
   }
 
-  @media only screen and (max-width: 920px) {
+  @media only screen and (max-width: 900px) {
+    .article {
+      margin-bottom: 0;
+    }
+    .category {
+      font-size: 10px;
+      line-height: 12px;
+      margin-bottom: 4px;
+    }
     .title {
-      font-size: 14px;
-      line-height: 16px;
+      font-size: 11px;
+      line-height: 13px;
+      margin-bottom: 4px;
+    }
+    .date {
+      font-size: 10px;
+      line-height: 12px;
     }
   }
 `;
@@ -224,7 +343,7 @@ const Category = ({
         <ContentWrapper>
           <PostsWrapper>
             {allWpPost.edges.map(({ node }) => (
-              <Post horizontal post={node} />
+              <PostCategory horizontal post={node} />
             ))}
           </PostsWrapper>
           <StyledAside>
@@ -236,6 +355,7 @@ const Category = ({
           </StyledAside>
         </ContentWrapper>
         <Pagination pageContext={pageContext} />
+        <NewsletterSmall />
       </Wrapper>
     </Layout>
   );
