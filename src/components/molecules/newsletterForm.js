9bd87051372c764e7ee10c67a9faebead633a1f0
Daniel <daniel.laskowski94@gmail.com>
2021-07-16 14:00:44 +0200

    add window object

--- src/components/molecules/newsletterForm.js
+++ src/components/molecules/newsletterForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { window } from "browser-monads";
 import styled from "styled-components";
 import SectionLink from "atoms/sectionLink";
 import Button from "atoms/button";
