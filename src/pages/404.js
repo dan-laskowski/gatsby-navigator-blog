5d9f9f08582e73ceb961d250cadb4e1a09716607
Daniel <daniel.laskowski94@gmail.com>
2021-07-16 11:07:13 +0200

    update media queries

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,6 +1,57 @@
 import * as React from "react";
+import { navigate } from "gatsby";
 import Seo from "molecules/seo";
+import styled from "styled-components";
 import Layout from "organisms/layout";
+import { Heading, Subheading } from "atoms/heading";
+import Button from "atoms/button";
+
+const Wrapper = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 300px;
+  height: 60vh;
+`;
+
+const StyledHeading = styled(Heading)`
+  font-size: 77px;
+  margin-bottom: 24px;
+  @media only screen and (max-width: 900px) {
+    font-size: 44px;
+    margin-bottom: 14px;
+  }
+`;
+
+const StyledSubheading = styled(Subheading)`
+  font-size: 24px;
+  line-height: 29px;
+  font-weight: 300;
+  margin-bottom: 58px;
+  text-align: center;
+  color: ${({ theme }) => theme.color.black};
+  @media only screen and (max-width: 900px) {
+    font-size: 14px;
+    line-height: 16px;
+    margin-bottom: 34px;
+  }
+`;
+
+const BackButton = styled(Button)`
+  width: 200px;
+  height: 60px;
+  font-family: ${({ theme }) => theme.font.heading.family};
+  background-color: ${({ theme }) => theme.color.black};
+  font-size: 24px;
+  line-height: 29px;
+  text-transform: uppercase;
+  @media only screen and (max-width: 900px) {
+    width: 116px;
+    height: 36px;
+    font-size: 12px;
+    line-height: 16px;
+  }
+`;
 
 const NotFoundPage = () => (
   <Layout>
@@ -8,8 +59,11 @@ const NotFoundPage = () => (
       title="Nie znaleziono strony"
       description="Niestety strona o podanym adresie nie istnieje!"
     />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Wrapper>
+      <StyledHeading text="och nie!" />
+      <StyledSubheading text="Strona, którą próbujesz otworzyć nie istnieje...smutek." />
+      <BackButton role="button" text="Wróć" onClick={() => navigate(-1)} />
+    </Wrapper>
   </Layout>
 );
 
